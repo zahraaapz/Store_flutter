9783d73662d33543ca15934957ed2e1fa5fc19ff
zahraaapz <zahrapirooz1379@gmail.com>
2023-08-08 12:55:06 +0330

    app store

--- lib/payment.dart
+++ lib/payment.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:appstore/address.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import 'color/color.dart';
@@ -19,7 +22,7 @@ class Payments extends StatelessWidget {
         backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
             children: [
             
             iconANDtitle('Payment', Icons.arrow_back_ios),
@@ -46,7 +49,7 @@ class Payments extends StatelessWidget {
              GestureDetector(
 
               onTap: () {
-                //to do:edit
+                //To do:edit
                 Get.offAll(Address());
               },
                child: Container(
@@ -64,8 +67,58 @@ class Payments extends StatelessWidget {
                 ],
               ),
 
-            )
-            
+            ),
+            SizedBox(height:10),
+            Container(
+              width:double.infinity,
+             height:50 ,
+              color: Rang.toosi,
+              child:Text('Product Detail')
+            ),SizedBox(height:10),
+             Container(
+              width:double.infinity,
+              height: 150,
+              color: Rang.toosi,
+              child: Column(
+                children: [
+SizedBox(height:15),
+                  Row(
+                    children: [
+                    SizedBox(width:9), 
+                   SvgPicture.asset('assets/google.svg',width: 30,height: 30,),
+                   SizedBox(width:10),
+                      Text('Google pay' ,)
+                    ], ),
+SizedBox(height:10),
+                    Row(
+                    children: [  
+                      SvgPicture.asset('assets/Paytm.svg',width: 35,height: 35,),
+                   SizedBox(width:4),
+
+                      Text('paytm' ,)
+                    ],) 
+                    ,SizedBox(height:10),
+                    Row(
+                    children: [  
+                     SizedBox(width:12),  
+                   SvgPicture.asset('assets/phonepe.svg',width: 30,height: 30,),
+                   SizedBox(width:12),
+
+                      Text('Phonepe' ,)
+                    ],)
+                  
+                
+                ],
+              )
+            ),
+               SizedBox(
+                  width: double.infinity,
+              height: 50,
+                 child: ElevatedButton(onPressed: (){}, style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
+                  backgroundColor: MaterialStateProperty.all(Rang.blue)),
+                             child:const Text('Pay now')),
+               )
             
         ]))));             
 }
